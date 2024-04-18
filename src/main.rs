21733082ee93064ff1dd719fd81227e58f285c2a
Tyler Harpool <tharpool@jackhenry.com>
2024-04-18 16:31:40 -0500

    Listen on 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ async fn main() {
 			get(provider),
 		);
 
-	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+	let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 	println!("Server listening on {}", addr);
 
 	axum::Server::bind(&addr)
