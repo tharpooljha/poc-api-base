9a4cbc2fc74ffc01f39757642fcc55d276f97c95
Tyler Harpool <tharpool@jackhenry.com>
2024-04-18 14:24:36 -0500

    Created smarter urls

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use axum::{routing::get, Router};
+use axum::{extract::Path, routing::get, Router};
 use std::net::SocketAddr;
 
-async fn screening() -> String {
-	"Screening result for 4e3cde86-37ea-48bb-adef-4f820ba70b0f".to_string()
+async fn screening(
+	Path(uuid): Path<String>,
+	Path(api_version): Path<String>,
+) -> String {
+	format!("Screening result for {} {}", api_version, uuid)
+}
+
+async fn provider(
+	Path(uuid): Path<String>,
+	Path(api_version): Path<String>,
+) -> String {
+	format!("Screening result for {} {}", api_version, uuid)
 }
 
 #[tokio::main]
 async fn main() {
-	let app = Router::new().route(
-		"/api/sanctions/v1/4e3cde86-37ea-48bb-adef-4f820ba70b0f/screening",
-		get(screening),
-	);
+	let app = Router::new()
+		.route(
+			"/api/sanctions/:api_version/:uuid/screening",
+			get(screening),
+		)
+		.route("/api/sanctions/:api_version/:uuid/provider", get(provider));
 
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 	println!("Server listening on {}", addr);
