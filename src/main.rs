b3ee2869d6a416ebcf754812f55c966a7fbb03b2
Tyler Harpool <tharpool@jackhenry.com>
2024-04-18 20:54:52 -0500

    Add healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
-use axum::{extract::Path, routing::get, Router};
+use axum::{
+	extract::Path, response::Html, response::IntoResponse, routing::get, Json,
+	Router,
+};
 use std::net::SocketAddr;
 
 async fn screening(
-	Path(uuid): Path<String>,
-	Path(api_version): Path<String>,
-) -> String {
-	format!("Screening result for {} {}", api_version, uuid)
+	Path((api_version, uuid)): Path<(String, String)>,
+) -> Html<String> {
+	let content = format!("<p>{}</p><p>{}</p>", api_version, uuid);
+	Html(content)
 }
 
 async fn provider(
-	Path(uuid): Path<String>,
-	Path(api_version): Path<String>,
-) -> String {
-	format!("Screening result for {} {}", api_version, uuid)
+	Path((api_version, uuid)): Path<(String, String)>,
+) -> Html<String> {
+	let content = format!("<p>{}</p><p>{}</p>", api_version, uuid);
+	Html(content)
+}
+
+async fn index() -> Html<&'static str> {
+	Html(
+		"<h1>poc-api-base!</h1> \n <p>API for POC</p>\n   <p>Version 0.1.0</p>\n
+	<p>Test:</p>\n  <p><a href='/api/poc-api-base/v1/4e3cde86-37ea-48bb-adef-4f820ba70b0f/screening'>Test</p>\n",
+	)
+}
+
+pub async fn health_check_handler() -> impl IntoResponse {
+	const MESSAGE: &str = "API Services";
+
+	let json_response = serde_json::json!({
+		"status": "ok",
+		"message": MESSAGE
+	});
+
+	Json(json_response)
 }
 
 #[tokio::main]
 async fn main() {
 	let app = Router::new()
+		.route("/", get(index))
 		.route(
 			"/api/poc-api-base/:api_version/:uuid/screening",
 			get(screening),
@@ -25,7 +47,8 @@ async fn main() {
 		.route(
 			"/api/poc-api-base/:api_version/:uuid/provider",
 			get(provider),
-		);
+		)
+		.route("/health", get(health_check_handler));
 
 	let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 	println!("Server listening on {}", addr);
